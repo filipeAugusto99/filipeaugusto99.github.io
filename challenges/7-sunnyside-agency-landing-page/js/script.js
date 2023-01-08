64745147fe24ddbf85079afeffcc34a20e6f5c2f
Filipe Augusto <104656805+filipeAugusto99@users.noreply.github.com>
2023-01-08 20:04:53 -0300

    landing-page
    mobile-first, faltando a parte do desktop

---
+++ challenges/7-sunnyside-agency-landing-page/js/script.js
@@ -0,0 +1,15 @@
+const menuMobile = document.querySelector('.navbar')
+const menu = document.querySelector('.menu-image')
+
+let open = false;
+
+menu.onclick = function() {
+  if(!open === true) {
+    open = true
+    menuMobile.classList.add('open')
+    return
+  }
+  open = false
+  menuMobile.classList.remove('open')
+}
+
